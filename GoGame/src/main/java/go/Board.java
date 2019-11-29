b0f56f2030694340a593919f1a9e4ade938336ee
Wojtaz99 <49129200+Wojtaz99@users.noreply.github.com>
2019-11-29 20:19:38 +0100

    blabla

--- GoGame/src/main/java/go/Board.java
+++ GoGame/src/main/java/go/Board.java
@@ -9,7 +9,7 @@ public class Board extends JPanel{
 	final int DIMX = 600, DIMY = 600;
 
 	public Board() {
-		this.setSize(new Dimension(dimX, dimY));
+		this.setSize(new Dimension(DIMX, DIMY));
 		repaint();
 	}
 
@@ -22,14 +22,14 @@ public class Board extends JPanel{
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
 		for (int i=0; i<SIZE; i++) { // pionowe linie
-				Point2D x = new Point2D.Double((dimX / SIZE) * i, 0);
-				Point2D y = new Point2D.Double((dimX / SIZE) * (i + 1), dimY);
+				Point2D x = new Point2D.Double((DIMX / SIZE) * i, 0);
+				Point2D y = new Point2D.Double((DIMX / SIZE) * (i + 1), DIMY);
 				Line2D line = new Line2D.Double(x,y);
 				g2d.draw(line);
 		}
 		for (int i=0; i<SIZE; i++) { // poziome linie
-			Point2D x = new Point2D.Double(0, (dimY / SIZE) * i);
-			Point2D y = new Point2D.Double(dimX, (dimX / SIZE) * (i + 1));
+			Point2D x = new Point2D.Double(0, (DIMY / SIZE) * i);
+			Point2D y = new Point2D.Double(DIMX, (DIMX / SIZE) * (i + 1));
 			Line2D line = new Line2D.Double(x,y);
 			g2d.draw(line);
 		}
