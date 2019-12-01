426d935d005eb6f09a466e4ac5dbbe79ab4b3916
Wojtaz99 <49129200+Wojtaz99@users.noreply.github.com>
2019-12-01 17:41:38 +0100

    Menu główne, z komentarzami, przerzucone do osobnej klasy juz dzialajace

--- GoGame/src/main/java/go/Main.java
+++ GoGame/src/main/java/go/Main.java
@@ -7,59 +7,41 @@ import java.awt.event.*;
 /** Głowna klasa gracza */
 public class Main extends JFrame implements ActionListener, MouseListener, MouseMotionListener {
 
-	static Main frame;
+	static Menu menu;
 	/** Pasek menu */
-	JMenuBar menuBar;
+	private JMenuBar menuBar;
 	/** Opcje na pasku menu */
-	JMenu optionsMenu;
+	private JMenu optionsMenu;
 	/** Info na pasku menu */
-	JMenu infoMenu;
+	private JMenu infoMenu;
 	/** Opcja nowa gra */
-	JMenuItem newGameItem;
+	private JMenuItem newGameItem;
 	/** Opcja wyjdz z gry */
-	JMenuItem exitItem;
+	private JMenuItem exitItem;
 	/** Opcja spasuj kolejke */
-	JMenuItem passItem;
+	private JMenuItem passItem;
 	/** Informacja o zasadach */
-	JMenuItem rulesItem;
+	private JMenuItem rulesItem;
 	/** Informacja o autorach */
-	JMenuItem authorsItem;
+	private JMenuItem authorsItem;
 	/** Plansza */
-	Board board;
+	private Board board;
 
-	/** Guzik rozpoczynajacy gre z botem */
-	JButton botStartButton;
-	/** Guzik rozpoczynajacy gre z graczem */
-	JButton playerStartButton;
-	/** Panele do menu */
-	JPanel menuPanel, titlePanel, descPanel, radioPanel, playerPanel, botPanel;
-	/** Etykieta z tytulem gry */
-	JLabel titleLabel;
-	/** Etykieta z opisem */
-	JLabel descLabel;
-	/** Grupa radio buttonow */
-	ButtonGroup buttonGroup;
-	/** Radio button plansza 19x19 */
-	JRadioButton big;
-	/** Radio button plansza 13x13 */
-	JRadioButton normal;
-	/** Radio button plansza 9x9 */
-	JRadioButton small;
-
-	/** Konstruktor maina */
-	Main() {
-		super("Go Game");
-			menuInitialize();
-	}
+	/** Konstruktor maina
+	 * @param gameSize rozmiar planszy
+	 * */
 	Main(int gameSize) {
 		gameInitialize(gameSize);
 	}
 
+	/** Funkcja main
+	 * @param args puste
+	 */
 	public static void main(String[] args) {
-		frame = new Main();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.setResizable(false);
+		menu = new Menu();
+		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menu.setVisible(true);
+		menu.setResizable(false);
 	}
 
 	/** Metoda actionPerformed */
@@ -70,86 +52,22 @@ public class Main extends JFrame implements ActionListener, MouseListener, Mouse
 		} else if (event == authorsItem) {
 			JOptionPane.showMessageDialog(null, "Authors: Wiktoria Byra, Wojciech Pakulski", "Authors", JOptionPane.INFORMATION_MESSAGE);
 		}
-		else if(event == playerStartButton) {
-			if (big.isSelected()) {
-				gameInitialize(19);
-			}
-
-			else if (normal.isSelected()) {
-				gameInitialize(13);
-			}
-			else if (small.isSelected()){
-				gameInitialize(9);
-			}
-		}
 	}
-	void menuInitialize() {
-		// ustawianie nazw buttonow i labeli
-		Font labelFont = new Font("Segoe UI", Font.PLAIN, 20);
-		botStartButton = new JButton("Start game against a computer");
-		botStartButton.setFont(labelFont);
-		playerStartButton = new JButton("Start game against player");
-		playerStartButton.setFont(labelFont);
-		titleLabel = new JLabel("Go Game");
-		titleLabel.setFont(new Font("Segoe UI", Font.BOLD, 20));
-		descLabel = new JLabel("To play, select the board size and mode");
-		descLabel.setFont(labelFont);
-		// ustawienie panelu glownego
-		menuPanel = new JPanel();
-		menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
 
-		// ustawienie radio buttonow
-		buttonGroup = new ButtonGroup();
-		big = new JRadioButton("19x19");
-		normal = new JRadioButton("13x13");
-		small = new JRadioButton("9x9");
-		Font radioFont = new Font("Segoe UI", Font.PLAIN, 16);
-		radioPanel = new JPanel();
-		buttonGroup.add(big);
-		radioPanel.add(big);
-		big.setFont(radioFont);
-		buttonGroup.add(normal);
-		radioPanel.add(normal);
-		normal.setFont(radioFont);
-		buttonGroup.add(small);
-		radioPanel.add(small);
-		small.setFont(radioFont);
-
-		// dodawanie elementow do ich paneli
-		titlePanel = new JPanel();
-		titlePanel.add(titleLabel);
-		descPanel = new JPanel();
-		descPanel.add(descLabel);
-		playerPanel = new JPanel();
-		playerPanel.add(playerStartButton);
-		botPanel = new JPanel();
-		botPanel.add(botStartButton);
-		// dodawanie paneli do panelu glownego
-		menuPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-		menuPanel.add(titlePanel);
-		menuPanel.add(Box.createRigidArea(new Dimension(0, 5)));
-		menuPanel.add(descPanel);
-		menuPanel.add(radioPanel);
-		menuPanel.add(playerPanel);
-		menuPanel.add(botPanel);
-		add(menuPanel);
-		botStartButton.addActionListener(this);
-		playerStartButton.addActionListener(this);
-		pack();
-	}
+	/** Tworzy okno gry */
 	void gameInitialize(int boardSize) {
 		// tworzenie menu
 		menuBar = new JMenuBar();
 		optionsMenu = new JMenu("Options");
 		passItem = new JMenuItem("Pass");
-		newGameItem = new JMenuItem("New Game");
+		newGameItem = new JMenuItem("Menu");
 		exitItem = new JMenuItem("Exit");
 		// dodawanie elementow do menu
 		optionsMenu.add(passItem);
 		optionsMenu.add(newGameItem);
 		optionsMenu.add(exitItem);
 		menuBar.add(optionsMenu);
-		frame.setJMenuBar(menuBar);
+		setJMenuBar(menuBar);
 		// menu info z opcjami zasady i autorzy
 		infoMenu = new JMenu("Info");
 		rulesItem = new JMenuItem("Rules");
@@ -157,12 +75,10 @@ public class Main extends JFrame implements ActionListener, MouseListener, Mouse
 		infoMenu.add(rulesItem);
 		infoMenu.add(authorsItem);
 		menuBar.add(infoMenu);
-		JPanel panel = new JPanel();
-		panel.add(board, BorderLayout.CENTER);
-		setLocationByPlatform(true);
 		board = new Board(boardSize);
-		frame.add(panel, BorderLayout.CENTER);
-		panel.setVisible(true);
+		add(board, BorderLayout.CENTER);
+		setLocationByPlatform(true);
+
 		// action Listenery
 		passItem.addActionListener(this);
 		newGameItem.addActionListener(this);
---
+++ GoGame/src/main/java/go/Menu.java
@@ -0,0 +1,114 @@
+package go;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/** Klasa obslugujaca menu glowne */
+class Menu extends JFrame implements ActionListener {
+
+    static Main frame;
+    /** Guzik rozpoczynajacy gre z botem */
+    private JButton botStartButton;
+    /** Guzik rozpoczynajacy gre z graczem */
+    private JButton playerStartButton;
+    /** Panele do menu */
+    private JPanel menuPanel, titlePanel, descPanel, radioPanel, playerPanel, botPanel;
+    /** Etykieta z tytulem gry */
+    private JLabel titleLabel;
+    /** Etykieta z opisem */
+    private JLabel descLabel;
+    /** Grupa radio buttonow */
+    private ButtonGroup buttonGroup;
+    /** Radio button plansza 19x19 */
+    private JRadioButton big;
+    /** Radio button plansza 13x13 */
+    private JRadioButton normal;
+    /** Radio button plansza 9x9 */
+    private JRadioButton small;
+
+    /** Konstruktor menu */
+    Menu(){
+        super("Go Menu");
+        // ustawianie nazw buttonow i labeli
+        Font labelFont = new Font("Segoe UI", Font.PLAIN, 20);
+        botStartButton = new JButton("Start game against a computer");
+        botStartButton.setFont(labelFont);
+        playerStartButton = new JButton("Start game against player");
+        playerStartButton.setFont(labelFont);
+        titleLabel = new JLabel("Go Game");
+        titleLabel.setFont(new Font("Segoe UI", Font.BOLD, 20));
+        descLabel = new JLabel("To play, select the board size and mode");
+        descLabel.setFont(labelFont);
+        // ustawienie panelu glownego
+        menuPanel = new JPanel();
+        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
+
+        // ustawienie radio buttonow
+        buttonGroup = new ButtonGroup();
+        big = new JRadioButton("19x19");
+        normal = new JRadioButton("13x13");
+        small = new JRadioButton("9x9");
+        Font radioFont = new Font("Segoe UI", Font.PLAIN, 16);
+        radioPanel = new JPanel();
+        buttonGroup.add(big);
+        radioPanel.add(big);
+        big.setFont(radioFont);
+        buttonGroup.add(normal);
+        radioPanel.add(normal);
+        normal.setFont(radioFont);
+        buttonGroup.add(small);
+        radioPanel.add(small);
+        small.setFont(radioFont);
+        small.setSelected(true);
+
+        // dodawanie elementow do ich paneli
+        titlePanel = new JPanel();
+        titlePanel.add(titleLabel);
+        descPanel = new JPanel();
+        descPanel.add(descLabel);
+        playerPanel = new JPanel();
+        playerPanel.add(playerStartButton);
+        botPanel = new JPanel();
+        botPanel.add(botStartButton);
+        // dodawanie paneli do panelu glownego
+        menuPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        menuPanel.add(titlePanel);
+        menuPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        menuPanel.add(descPanel);
+        menuPanel.add(radioPanel);
+        menuPanel.add(playerPanel);
+        menuPanel.add(botPanel);
+        add(menuPanel);
+        botStartButton.addActionListener(this);
+        playerStartButton.addActionListener(this);
+        pack();
+    }
+    /** Metoda actionPerformed */
+    public void actionPerformed(ActionEvent actionEvent) {
+        Object event = actionEvent.getSource();
+        if(event == playerStartButton) {
+            if (big.isSelected()) {
+                frame = new Main(19);
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setVisible(true);
+                setResizable(false);
+                this.dispose();
+            }
+            else if (normal.isSelected()) {
+                frame = new Main(13);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+                frame.setResizable(false);
+                this.dispose();
+            }
+            else if (small.isSelected()){
+                frame = new Main(9);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+                frame.setResizable(false);
+                this.dispose();
+            }
+        }
+    }
+}
