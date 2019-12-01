1ba3db603d71896ad409b7baf295de10e3692ed0
byrozaurowe <wiktoriabyra99@gmail.com>
2019-12-01 14:20:35 +0100

    rozmieszczenie punktów strategicznych przy innych wymiarach planszy, większy margines

--- GoGame/src/main/java/go/Board.java
+++ GoGame/src/main/java/go/Board.java
@@ -10,24 +10,26 @@ import java.awt.geom.Point2D;
 public class Board extends JPanel{
 
     /** Ilosc pol nq planszy w pionie/poziomie */
-	final int SIZE = 13;
+	int SIZE = 13;
 	/** Promien kamieni */
 	final static int STONERADIUS = 15;
 	/** Rozmiar planszy w pixelach */
-	final int DIMX = 600, DIMY = 600;
+	final double DIMX = 600, DIMY = 600;
+	final int MARGIN = 2*STONERADIUS;
 	/** Tablica z kamieniami na planszy */
 	static Stone[][] boardTab;
 	/** Ostatni tymczasowy kamień */
 	Stone lastMovedField;
 
 	/** Konstruktor planszy */
-	public Board() {
+	public Board(int size) {
+		SIZE = size;
 		setBackground(new Color(193,154,107));
-		this.setPreferredSize(new Dimension(DIMX+2*STONERADIUS, DIMY+2*STONERADIUS));
+		this.setPreferredSize(new Dimension((int)DIMX+2*MARGIN, (int)DIMY+2*MARGIN));
 		boardTab = new Stone [SIZE][SIZE];
 		for (int i=0; i<SIZE; i++) {
 			for (int j=0; j<SIZE; j++) {
-				boardTab[i][j] = new Stone(i,j,(i*DIMX/(SIZE-1))+STONERADIUS,(j*DIMY/(SIZE-1))+STONERADIUS);
+				boardTab[i][j] = new Stone(i,j,(i*DIMX/(SIZE-1))+MARGIN,(j*DIMY/(SIZE-1))+MARGIN);
 			}
 		}
 		repaint();
@@ -102,27 +104,39 @@ public class Board extends JPanel{
 		g.drawImage(bgImage, 0, 0, null);
 
 		for (int i=0; i<SIZE; i++) { // pionowe linie
-				Point2D x = new Point2D.Double((DIMX / (SIZE-1)) * i + STONERADIUS, STONERADIUS);
-				Point2D y = new Point2D.Double((DIMY / (SIZE-1)) * i + STONERADIUS, DIMY+STONERADIUS);
+				Point2D x = new Point2D.Double((DIMX / (SIZE-1)) * i + MARGIN, MARGIN);
+				Point2D y = new Point2D.Double((DIMY / (SIZE-1)) * i + MARGIN, DIMY+MARGIN);
 				Line2D line = new Line2D.Double(x,y);
 				g2d.setPaint(Color.DARK_GRAY);
 				g2d.setStroke(new BasicStroke(3));
 				g2d.draw(line);
 		}
 		for (int i=0; i<SIZE; i++) { // poziome linie
-			Point2D x = new Point2D.Double(STONERADIUS, (DIMY / (SIZE-1)) * i + STONERADIUS);
-			Point2D y = new Point2D.Double(DIMX + STONERADIUS, (DIMX / (SIZE-1)) * i + STONERADIUS);
+			Point2D x = new Point2D.Double(MARGIN, (DIMY / (SIZE-1)) * i + MARGIN);
+			Point2D y = new Point2D.Double(DIMX + MARGIN, (DIMX / (SIZE-1)) * i + MARGIN);
 			Line2D line = new Line2D.Double(x,y);
 			g2d.setPaint(Color.DARK_GRAY);
 			g2d.setStroke(new BasicStroke(3));
 			g2d.draw(line);
 		}
-		for (int i=0; i<3; i++) { //rysowanie punktów strategicznych
-			for (int j=0; j<3; j++) {
-				Ellipse2D circle = new Ellipse2D.Double(DIMX/4*(i+1)-5+STONERADIUS, DIMY/4*(j+1)-5+STONERADIUS, 10, 10);
-				g2d.setPaint(Color.DARK_GRAY);
-				g2d.draw(circle);
-				g2d.fill(circle);
+		if (SIZE == 13 || SIZE == 9) {
+			for (int i = 0; i < 3; i++) { //rysowanie punktów strategicznych
+				for (int j = 0; j < 3; j++) {
+					Ellipse2D circle = new Ellipse2D.Double(DIMX / 4 * (i + 1) - 5 + MARGIN, DIMY / 4 * (j + 1) - 5 + MARGIN, 10, 10);
+					g2d.setPaint(Color.DARK_GRAY);
+					g2d.draw(circle);
+					g2d.fill(circle);
+				}
+			}
+		}
+		else if (SIZE == 19) {
+			for (int i = 0; i < 5; i+=2) { //rysowanie punktów strategicznych
+				for (int j = 0; j < 5; j+=2) {
+					Ellipse2D circle = new Ellipse2D.Double(DIMX / 6 * (i + 1) - 5 + MARGIN, DIMY / 6 * (j + 1) - 5 + MARGIN, 10, 10);
+					g2d.setPaint(Color.DARK_GRAY);
+					g2d.draw(circle);
+					g2d.fill(circle);
+				}
 			}
 		}
 		for (int i=0; i<SIZE; i++) { //rysowanie kamieni
