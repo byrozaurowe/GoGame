e73c7b70555269557b28f705c0d5e8fa71182e6b
Wojtaz99 <49129200+Wojtaz99@users.noreply.github.com>
2019-12-17 14:35:57 +0100

    Zakonczenie gry bez bledu jak gracz sie rozlaczy/podda. Poprawne odczytywanie z serwera informacji o jencach. Komentarze

--- GoGame/src/main/java/GUI.java
+++ GoGame/src/main/java/GUI.java
@@ -28,29 +28,14 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
     private int playerID;
     /** Etykieta wyswietlajaca stan gry */
     private JLabel gameStatusLabel;
+    /** Twoi jency */
+    private int captives;
 
     /** Konstruktor maina
      * @param boardSize rozmiar planszy
      * */
     GUI(int boardSize) {
         gameInitialize(boardSize);
-    }
-
-    /** Zwroc msg
-     * @return wiadomosc do serwera
-     */
-    String getMsg() {
-        return msg;
-    }
-
-    /** Ustaw msg na null */
-    void nullMsg() {
-        msg = null;
-    }
-
-    /** Ustaw id gracza */
-    void setPlayerID(int playerID) {
-        this.playerID = playerID;
     }
 
     /** Metoda actionPerformed */
@@ -73,7 +58,7 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
         optionsMenu = new JMenu("Options");
         passItem = new JMenuItem("Pass");
         newGameItem = new JMenuItem("Menu");
-        exitItem = new JMenuItem("Exit");
+        exitItem = new JMenuItem("Surrender/Exit");
         // dodawanie elementow do menu
         optionsMenu.add(passItem);
         optionsMenu.add(newGameItem);
@@ -115,7 +100,7 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
 
     /** Pokaz podsumowanie jencow na koncu gry */
     void showSummary() {
-        JOptionPane.showMessageDialog(this, "You have " + GameClient.gameClient.getCaptives() + " captives \n",
+        JOptionPane.showMessageDialog(this, "You have " + captives + " captives \n",
                 "Results", JOptionPane.INFORMATION_MESSAGE);
     }
 
@@ -124,11 +109,14 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
      * */
     int doYouWantToEnd() {
         return JOptionPane.showConfirmDialog(this,
-                "You have " + GameClient.gameClient.getCaptives() + " captives \n"
+                "You have " + captives + " captives \n"
                         + "Do you want to resume the game?", "Is this the end?",
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
     }
 
+    /** Ustawia wyglad planszy analizujac wiadomosc od serwera
+     * @param line wiadomosc o stanie gry od serwera
+     * */
     void setBoard(String line) {
         for (int i = 0; i < board.getSIZE(); i++) {
             for (int j = 0; j < board.getSIZE(); j++) {
@@ -143,20 +131,22 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
                 }
             }
         }
+        String[] dane = line.split(" ");
+        if(playerID == 1)
+            captives = Integer.parseInt(dane[1]);
+        else captives = Integer.parseInt(dane[2]);
         repaint();
     }
 
     /** Okno mowiace ze przeciwnik podejmuje decyzje */
     void waitForOpponent() {
-        JOptionPane.showMessageDialog(this, "You have " + GameClient.gameClient.getCaptives() + " captives \n" +
+        JOptionPane.showMessageDialog(this, "You have " + captives + " captives \n" +
                 "Waiting for opponent", "Results - waiting...", JOptionPane.INFORMATION_MESSAGE);
     }
 
-    /** Zwroc obiekt plansza
-     * @return plansza
-     * */
-    Board getBoard () {
-        return board;
+    /** Okno mowiace ze przeciwnik podejmuje decyzje */
+    void opponentLeft() {
+        JOptionPane.showMessageDialog(this, "You win! Opponent left or surrendered", "You win!", JOptionPane.INFORMATION_MESSAGE);
     }
 
     /** Ustaw tabele statusu gry
@@ -176,12 +166,35 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
         gameStatusLabel.setText(text);
     }
 
+    /** Sprawdza czy jest twoja tura
+     * @return czy jest twoja tura
+     */
     boolean checkIfIsYourTurn() {
         return GameClient.gameClient.isYourTurn;
     }
 
+    /** Sprawdza czy gra juz sie skonczyla
+     * @return czy gra sie skonczyla
+     * */
     boolean checkIfGameIsFinished() {
         return GameClient.gameClient.gameIsFinished;
+    }
+
+    /** Zwroc msg
+     * @return wiadomosc do serwera
+     */
+    String getMsg() {
+        return msg;
+    }
+
+    /** Ustaw msg na null */
+    void nullMsg() {
+        msg = null;
+    }
+
+    /** Ustaw id gracza */
+    void setPlayerID(int playerID) {
+        this.playerID = playerID;
     }
 
     /** Implementowana funkcja mouse listenera */
@@ -214,15 +227,17 @@ public class GUI extends JFrame implements ActionListener, MouseListener, MouseM
     @Override
     public void mouseMoved(MouseEvent mouseEvent) {}
 
+    /** Ustaw msg (do testow) */
     void setMsg(String msg) { this.msg = msg;}
 
+    /** Zwroc id gracza (do testow)
+     * @return id gracza
+     * */
     int getPlayerID() { return playerID; }
 
-    Player getPlayerColor() {
-        if(getPlayerID() == 1) return Player.BLACK;
-        else return Player.WHITE;
-    }
-
+    /** Zwroc co wypisuje etykieta statusu (do testow)
+     * @return tekst na etykiecie statusu
+     * */
     String getLabelText() {
         return gameStatusLabel.getText();
     }
