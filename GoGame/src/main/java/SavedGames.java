0118a3bc3142f2586df555e538a8b62fb0a888f8
Wojtaz99 <49129200+Wojtaz99@users.noreply.github.com>
2020-01-17 00:53:06 +0100

    Data wreszcie dziala

--- GoGame/src/main/java/SavedGames.java
+++ GoGame/src/main/java/SavedGames.java
@@ -6,15 +6,14 @@ import java.util.Date;
 @Entity
 //@org.hibernate.annotations.Entity(optimisticLock = OptimisticLockType.ALL)
 @Table(name = "savedgames", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "id"),
-        @UniqueConstraint(columnNames = "gameDate") })
+        @UniqueConstraint(columnNames = "id") })
 public class SavedGames {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true, nullable = false)
     private int id;
 
-    @Column(name = "gameDate", unique = true, nullable = false)
+    @Column(name = "gameDate", unique = true)
     private Date date;
 
     public SavedGames() {
