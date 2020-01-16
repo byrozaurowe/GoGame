4cd9277b3931bdf717bde301a0eb8ce4a774a579
Wojtaz99 <49129200+Wojtaz99@users.noreply.github.com>
2020-01-16 13:36:39 +0100

    Dodawanie nowej gry do tabeli w bazie

--- GoGame/src/main/java/DatabaseApp.java
+++ GoGame/src/main/java/DatabaseApp.java
@@ -1,21 +1,32 @@
 import org.hibernate.*;
-import org.hibernate.cfg.*;
-
 import java.util.Date;
+import java.util.List;
 
 class DatabaseApplication {
     public static void main(String[] args) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+        if(args[0].equals("newGame")) {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
 
-        // Add new Employee object
-        SavedGames savedGames = new SavedGames();
-        savedGames.setId(6);
-        savedGames.setDate(new Date(20,12,2001));
+            SavedGames savedGames = new SavedGames();
+            Date date = new Date();
+            java.sql.Date sDate = new java.sql.Date(date.getTime());
+            savedGames.setDate(sDate);
 
-        session.save(savedGames);
+            session.save(savedGames);
 
-        session.getTransaction().commit();
-        HibernateUtil.shutdown();
+            session.getTransaction().commit();
+        }
+        else {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query query = session.createQuery("SELECT COUNT(*) FROM savedgames");
+            List result = query.list();
+            OneGame oneGame = new OneGame();
+            oneGame.setMoveId(Integer.parseInt(args[0]));
+            oneGame.setMoveString(args[1]);
+            session.save(oneGame);
+            session.getTransaction().commit();
+        }
     }
 }
