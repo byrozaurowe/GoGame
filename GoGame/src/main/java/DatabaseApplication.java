ab841f01e7af961ffec5ca8f338b77617fb16db2
Wojciech Pakulski <wojtekpakulski@gmail.com>
2020-01-22 17:12:34 +0100

    Mała korekta errora

--- GoGame/src/main/java/DatabaseApplication.java
+++ GoGame/src/main/java/DatabaseApplication.java
@@ -1,3 +1,4 @@
+import org.apache.log4j.varia.NullAppender;
 import org.hibernate.*;
 import org.hibernate.boot.model.relational.Database;
 
@@ -7,48 +8,56 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+/** Klasa kwerend */
 class DatabaseApplication {
 
     DatabaseApplication() {}
 
-    public static List queries(String[] args) {
+    /** Metoda egzekwujaca kwerendy */
+     static List queries(String[] args) {
+        org.apache.log4j.BasicConfigurator.configure(new NullAppender());
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
+        List result = null;
         if(args[0].equals("date list")) {
             Query query = session.createSQLQuery("SELECT DATE_SUB(gameDate, INTERVAL 1 HOUR ) FROM SavedGames");
-            List result = query.list();
-            return result;
-        }
-        if(args[0].equals("newGame")) {
-            SavedGames savedGames = new SavedGames();
-            Date date = new Date();
-            Calendar cal = Calendar.getInstance(); // creates calendar
-            cal.setTime(date); // sets calendar time/date
-            cal.add(Calendar.HOUR, 1); // adds one hour
-            Date data = cal.getTime();
-            java.sql.Date sDate = new java.sql.Date(data.getTime());
-            savedGames.setDate(sDate);
-            session.save(savedGames);
-        }
-        else if(args[0].equals("data")) {
-
-            Query query = null;
-            query = session.createQuery("SELECT id FROM SavedGames WHERE date = '" + args[1] + "'");
-            List result = query.list();
-            query = session.createQuery("SELECT moveString FROM OneGame WHERE gameId = " + result.get(0) + " ORDER BY id");
             result = query.list();
-            return result;
         }
-        else if(args[0].equals("list of moves")) {
-            Query query = session.createQuery("SELECT COUNT(*) FROM SavedGames");
-            List result = query.list();
-            OneGame oneGame = new OneGame();
-            oneGame.setGameId(((Long) result.get(0)).intValue());
-            oneGame.setMoveId(Integer.parseInt(args[1]));
-            oneGame.setMoveString(args[2]);
-            session.save(oneGame);
-        }
+         switch (args[0]) {
+             case "newGame":
+                 SavedGames savedGames = new SavedGames();
+                 Date date = new Date();
+                 Calendar cal = Calendar.getInstance(); // creates calendar
+
+                 cal.setTime(date); // sets calendar time/date
+
+                 cal.add(Calendar.HOUR, 1); // adds one hour
+
+                 Date data = cal.getTime();
+                 java.sql.Date sDate = new java.sql.Date(data.getTime());
+                 savedGames.setDate(sDate);
+                 session.save(savedGames);
+                 break;
+             case "data": {
+                 Query query = session.createQuery("SELECT id FROM SavedGames WHERE date = '" + args[1] + "'");
+                 result = query.list();
+                 query = session.createQuery("SELECT moveString FROM OneGame WHERE gameId = :gameId ORDER BY id");
+                 query.setParameter("gameId", result.get(0));
+                 result = query.list();
+                 break;
+             }
+             case "doMove": {
+                 Query query = session.createQuery("SELECT COUNT(*) FROM SavedGames");
+                 result = query.list();
+                 OneGame oneGame = new OneGame();
+                 oneGame.setGameId(Integer.parseInt(String.valueOf(result.get(0))));
+                 oneGame.setMoveId(Integer.parseInt(args[1]));
+                 oneGame.setMoveString(args[2]);
+                 session.save(oneGame);
+                 break;
+             }
+         }
         session.getTransaction().commit();
-        return null;
+        return result;
     }
 }
